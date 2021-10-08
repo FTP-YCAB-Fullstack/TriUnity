d19f6bb4f6c721eafd682d5e14d73b10e11f588a
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-08 17:04:43 +0700

    Membuat model user

---
+++ server/models/User.js
@@ -0,0 +1,39 @@
+const Schema = require("mongoose").Schema;
+
+const validateEmail = email => {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const User = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validateEmail, "Please fill a valid email address"]
+  },
+  password: {
+    type: String,
+    required: true,
+    min: [6, "Please fill a password min 6 character"],
+    validate: {
+      validator: password => {
+        const character = /[A-Z]+/.test(password) && /[a-z]+/.test(password);
+        const number = /[0-9]+/.test(password);
+        return character && number;
+      }
+    }
+  }
+});
+
+module.exports = db => {
+  return db.model("User", User);
+};
