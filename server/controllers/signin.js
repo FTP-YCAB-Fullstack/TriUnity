c982d643b19239f3f549805a30a5987d5adc4fa5
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-08 21:38:43 +0700

    Membuat controller login

--- server/controllers/signin.js
+++ server/controllers/signin.js
@@ -26,7 +26,26 @@ class SignIn {
     });
   }
 
-  static async Login(req, res, next) {}
+  static async Login(req, res, next) {
+    const { email, password } = req.body;
+    const data = User.findOne({ email });
+    if (!data || !bcrypt.compareSync(password, data.password)) {
+      res.status(400).json({
+        message: "email or password is wrong"
+      });
+    } else {
+      const token = jwt.sign({ id: data._id }, process.env.SECRET_JWT_TOKEN);
+      res.status(200).json({
+        message: "success login",
+        token,
+        user: {
+          firstName: data.firstName,
+          lastName: data.lastName,
+          email
+        }
+      });
+    }
+  }
 }
 
 module.exports = SignIn;
