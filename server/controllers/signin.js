b9c7da65d24a5a5c140a5b8d6a560a7191c41c0d
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-08 21:31:02 +0700

    Menambahkan jwt dan bcrypt pada register

--- server/controllers/signin.js
+++ server/controllers/signin.js
@@ -1,16 +1,28 @@
-const axios = require("axios");
-let User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 class SignIn {
   static async Register(req, res, next) {
     const { firstName, lastName, email, password } = req.body;
-
-    const user = await User.create({ firstName, lastName, email, password });
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: bcrypt.hashSync(password, 8)
+    });
     const data = await user.save();
+    const token = jwt.sign({ id: data._id }, process.env.SECRET_JWT_TOKEN);
 
     res.status(201).json({
       message: "success register",
-      data
+      token,
+      user: {
+        id: data._id,
+        firstName,
+        lastName,
+        email
+      }
     });
   }
 
