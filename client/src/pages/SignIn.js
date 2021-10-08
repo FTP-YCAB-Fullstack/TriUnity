964a755766e06dead95525b277cb362ebc02283b
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-08 22:09:28 +0700

    Menambahkan cors pada server dan juga membuat login server sudah bisa di lakukan

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,18 +1,21 @@
 import React from "react";
+import Login from "../components/Login";
+import axios from "axios";
 
 const SignIn = () => {
+  const onSubmitLogin = async event => {
+    event.preventDefault();
+
+    const { data } = await axios.post("http://localhost:5000/users/login", {
+      email: event.target.email.value,
+      password: event.target.password.value
+    });
+    console.log(data);
+  };
+
   return (
     <>
-      <form>
-        <h2>Login</h2>
-        <input type="text" name="email" placeholder="insert email in here..." />
-        <input
-          type="password"
-          name="password"
-          placeholder="insert password in here..."
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <Login onSubmitLogin={onSubmitLogin} />
       <form>
         <h2>Register</h2>
         <input
--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mainRouter = require("./routes");
 const port = process.env.SERVER_PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
