dbbf8fbd513e746c6d2fafa13b53b1f3c6c3c382
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-08 21:39:46 +0700

    Membuat route login

--- server/routes/signin.js
+++ server/routes/signin.js
@@ -2,5 +2,6 @@ const signInRouter = require("express").Router();
 const signInController = require("../controllers/signin");
 
 signInRouter.post("/users/register", signInController.Register);
+signInRouter.post("/users/login", signInController.Login);
 
 module.exports = signInRouter;
