30b816c3bd7223b1171e54692ccabad78c17d3fd
AlimMakruf <alimm.makruf@gmail.com>
2021-10-10 14:46:12 +0700

    homepage

---
+++ client/src/pages/homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function homepage() {
+
+    const getData = () => {
+        fetch("http://localhost:3500/photos")
+    }
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default homepage
