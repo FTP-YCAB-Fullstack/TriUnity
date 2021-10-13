7a557b3b879a87b82bc78d4e25ae8604b8fab3c4
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-13 14:34:37 +0700

    Memperbarui image controller

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -4,29 +4,37 @@ const ImageForSale = require("../models/ImageForSell");
 
 class Image {
   static async uploadImage(req, res, next) {
-    const tempPath = req.file.path;
-    const filename = `${Date.now()}-${req.body.filename}`;
-    const targetPath = path.join(__dirname, `../data/image/${filename}`);
+    try {
+      const tempPath = req.file.path;
+      const filename = `${Date.now()}-${req.body.filename}`;
+      const { title, price, description } = req.body;
+      const targetPath = path.join(__dirname, `../data/image/${filename}`);
 
-    if (!path.extname(req.file.originalname).toLowerCase() === ".png") {
-      next({ code: 403, message: "Only .png files are allowed!" });
-      fs.unlink(tempPath, err => {
-        console.error(err);
-      });
-    } else {
-      fs.rename(tempPath, targetPath, err => {
-        if (err) {
-          next(err);
-        } else {
-          ImageForSale.create({
-            userId: req.currentUser._id,
-            image: filename
-          });
-          res.status(200).json({
-            message: "Image uploaded"
-          });
-        }
-      });
+      if (!path.extname(req.file.originalname).toLowerCase() === ".png") {
+        next({ code: 403, message: "Only .png files are allowed!" });
+        fs.unlink(tempPath, err => {
+          console.error(err);
+        });
+      } else {
+        fs.rename(tempPath, targetPath, err => {
+          if (err) {
+            next(err);
+          } else {
+            ImageForSale.create({
+              userId: req.currentUser._id,
+              image: filename,
+              title,
+              price,
+              description
+            });
+            res.status(200).json({
+              message: "Image uploaded"
+            });
+          }
+        });
+      }
+    } catch (error) {
+      next(error);
     }
   }
 }
