ef55c92ff25c29f2d6cd23c43311e890099a7c03
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-13 20:44:42 +0700

    Merapihkan tampilan dari jual photo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -31,7 +31,12 @@ function Navbar(props) {
   return (
     <nav>
       <div className="flex justify-between gap-5 py-2 px-4 items-center bg-red-600">
-        <img src={AssetLogo} alt="logo" onClick={onClicktoHome} />
+        <img
+          className="cursor-pointer"
+          src={AssetLogo}
+          alt="logo"
+          onClick={onClicktoHome}
+        />
         <div>
           <button onClick={onClicktoKeranjang} className="px-4 text-white">
             Keranjang
