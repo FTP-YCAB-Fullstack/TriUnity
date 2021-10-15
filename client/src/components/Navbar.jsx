52d2802eb459b2f34e4386a1bc0d56662effd62d
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-15 22:16:43 +0700

    Membuat photos for sale bisa di klik lewat home

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 import AssetLogo from "../assets/logo.png";
 import { useCookies } from "react-cookie";
-import Keranjang from '../assets/keranjang.png'
+import Keranjang from "../assets/keranjang.png";
 import { withRouter } from "react-router-dom";
 
 // function Navbar(props) {
@@ -10,15 +10,13 @@ import { withRouter } from "react-router-dom";
 //         <nav>
 //             <div className="flex justify-between gap-5 py-2 px-4 items-center bg-red-600">
 //                 <div className="flex flex-row gap-2">
-                
+
 //                 {/* <button  className="px-4 text-white">Keranjang</button> */}
 //                 {cookies.token ? <button onClick={props.onClickLogout} className="px-4 text-white">Logout</button> : <button onClick={props.onClicktoLogin} className="px-4 text-white">Login</button>}
 //                 </div>
 //             </div>
 //         </nav>
 //     )
-
-
 
 function Navbar(props) {
   const [cookies, , removeCookies] = useCookies(["token"]);
@@ -45,6 +43,12 @@ function Navbar(props) {
     removeCookies("token");
   };
 
+  const onClicktoPhotosForSale = () => {
+    props.history.push({
+      pathname: "/photos-for-sale"
+    });
+  };
+
   return (
     <nav>
       <div className="flex justify-between gap-5 py-2 px-4 items-center bg-red-600">
@@ -55,7 +59,20 @@ function Navbar(props) {
           onClick={onClicktoHome}
         />
         <div className="flex flex-row gap-2">
-          <img src={Keranjang} alt="Masukkan kedalam keranjang" className="cursor-pointer" onClick={onClicktoKeranjang}/>
+          <img
+            src={Keranjang}
+            alt="Masukkan kedalam keranjang"
+            className="cursor-pointer"
+            onClick={onClicktoKeranjang}
+          />
+          {cookies.token ? (
+            <button
+              className="px-4 text-white"
+              onClick={onClicktoPhotosForSale}
+            >
+              photos for sale
+            </button>
+          ) : null}
           {cookies.token ? (
             <button onClick={onClickLogout} className="px-4 text-white">
               Logout
