bf06bf90e924b854c79b3dff8b9777cb87a1f425
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-15 20:30:25 +0700

    Membuat photos for sale page request ke server

--- client/src/pages/PhotosForSale.js
+++ client/src/pages/PhotosForSale.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import ItemPhotoSale from "../components/ItemPhotoSale";
+import axios from "axios";
 
 const PhotosForSale = props => {
+  const [photos, setPhotos] = useState([]);
+  console.log(photos);
+
+  const getPhotos = async () => {
+    const response = await axios
+      .get("http://localhost:5000/image/for-sale", { withCredentials })
+      .catch(error => error.response);
+    if (response && response.status === 200) {
+      setPhotos(response.data.photos);
+    } else {
+      console.log(response);
+    }
+  };
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
   return (
     <>
       <Navbar />
