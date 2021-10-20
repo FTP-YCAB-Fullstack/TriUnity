152ed139f24f9912d9c2b14a3c891937782c7a2d
Mulia16 <muliafirmansyah16@gmail.com>
2021-10-20 17:46:24 +0700

    Menghapus collection, membuat photos on sale dapat di click

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -69,9 +69,17 @@ class Image {
       fs.unlink(path.join(__dirname, `../data/image/${id}`), async err => {
         if (err) return next(err);
         await ImageForSale.deleteOne({ image: id });
+        const splitFilename = filename.split(".");
+        splitFilename.pop();
         res.status(200).json({
           message: "Success deleting photos for sale"
         });
+        fs.unlink(
+          __dirname,
+          "../data/image",
+          splitFilename.join("") + "-watermark.png",
+          () => ""
+        );
       });
     } catch (error) {
       next(error);
